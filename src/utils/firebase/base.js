c94a61528da4142d058344c65e00e7439cc3c5d8
yuslianto <yuslianto56@gmail.com>
2022-10-15 09:29:17 +0700

    solve error firebase api key with npm start after edit file env

--- src/utils/firebase/base.js
+++ src/utils/firebase/base.js
@@ -1,22 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCxpCryZKqT5iJgEWaV9cb7Ra8di_xoUKg",
-  authDomain: "rea4c-auth-2fd2b.firebaseapp.com",
-  projectId: "rea4c-auth-2fd2b",
-  storageBucket: "rea4c-auth-2fd2b.appspot.com",
-  messagingSenderId: "48322876722",
-  appId: "1:48322876722:web:8185c3db2d05bfad2ab32b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig)
